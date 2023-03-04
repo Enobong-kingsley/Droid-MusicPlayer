cc65b5fde6d6fabf92111ba465a5841399ba102b
Percyking <enobongndedde@gmail.com>
2023-03-04 13:21:41 +0100

    playlist_modification

--- lib/controllers/player_controller.dart
+++ lib/controllers/player_controller.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/gestures.dart';
 import 'package:get/get.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class PlayerController extends GetxController {
   final audioQuery = OnAudioQuery();
+  final audioPlayer = AudioPlayer();
+
+  var playIndex = 0.obs;
+  var isPlaying = false.obs;
 
   @override
   void onInit() {
@@ -13,15 +18,30 @@ class PlayerController extends GetxController {
     checkPermission();
   }
 
+  playSong(String? uri, index) {
+    playIndex.value = index;
+    try {
+      audioPlayer.setAudioSource(
+        AudioSource.uri(
+          Uri.parse(uri!),
+        ),
+      );
+      audioPlayer.play();
+      isPlaying(true);
+    } on Exception catch (e) {
+      print(e.toString());
+    }
+  }
+
   checkPermission() async {
     var perm = await Permission.storage.request();
 
     if (perm.isGranted) {
-      return audioQuery.querySongs(
-          ignoreCase: true,
-          orderType: OrderType.ASC_OR_SMALLER,
-          sortType: null,
-          uriType: UriType.EXTERNAL);
+      // return audioQuery.querySongs(
+      //     ignoreCase: true,
+      //     orderType: OrderType.ASC_OR_SMALLER,
+      //     sortType: null,
+      //     uriType: UriType.EXTERNAL);
     } else {
       checkPermission();
     }
--- lib/database.dart
+++ lib/database.dart
@@ -38,7 +38,79 @@ List<Song> mostPopular = [
       image: "assets/images/artist5.jpeg",
       singer: "ley you",
       duration: 264,
-      color: Colors.blue)
+      color: Colors.blue),
+  Song(
+      name: "your power",
+      image: "assets/images/artist1.jpeg",
+      singer: "Billie Eilish",
+      duration: 300,
+      color: Colors.grey),
+  Song(
+      name: "Happiest Year",
+      image: "assets/images/artist3.jpeg",
+      singer: "Jaymes young",
+      duration: 252,
+      color: Colors.red),
+  Song(
+      name: "influence",
+      image: "assets/images/artist2.jpeg",
+      singer: "chris brown",
+      duration: 532,
+      color: Colors.orange),
+  Song(
+      name: "idk",
+      image: "assets/images/artist5.jpeg",
+      singer: "ley you",
+      duration: 264,
+      color: Colors.blue),
+  Song(
+      name: "your power",
+      image: "assets/images/artist1.jpeg",
+      singer: "Billie Eilish",
+      duration: 300,
+      color: Colors.grey),
+  Song(
+      name: "Happiest Year",
+      image: "assets/images/artist3.jpeg",
+      singer: "Jaymes young",
+      duration: 252,
+      color: Colors.red),
+  Song(
+      name: "influence",
+      image: "assets/images/artist2.jpeg",
+      singer: "chris brown",
+      duration: 532,
+      color: Colors.orange),
+  Song(
+      name: "idk",
+      image: "assets/images/artist5.jpeg",
+      singer: "ley you",
+      duration: 264,
+      color: Colors.blue),
+  Song(
+      name: "your power",
+      image: "assets/images/artist1.jpeg",
+      singer: "Billie Eilish",
+      duration: 300,
+      color: Colors.grey),
+  Song(
+      name: "Happiest Year",
+      image: "assets/images/artist3.jpeg",
+      singer: "Jaymes young",
+      duration: 252,
+      color: Colors.red),
+  Song(
+      name: "influence",
+      image: "assets/images/artist2.jpeg",
+      singer: "chris brown",
+      duration: 532,
+      color: Colors.orange),
+  Song(
+      name: "idk",
+      image: "assets/images/artist5.jpeg",
+      singer: "ley you",
+      duration: 264,
+      color: Colors.blue),
 ];
 List<Song> newlyReleased = [
   Song(
@@ -64,5 +136,77 @@ List<Song> newlyReleased = [
       image: "assets/images/artist3.jpeg",
       singer: "ley you",
       duration: 264,
-      color: Colors.blue)
+      color: Colors.blue),
+  Song(
+      name: "like it doesn't hurt",
+      image: "assets/images/artist4.jpeg",
+      singer: "Charle Cardin",
+      duration: 300,
+      color: Colors.grey),
+  Song(
+      name: "love on the brain",
+      image: "assets/images/artist6.jpeg",
+      singer: "Tvorchi",
+      duration: 252,
+      color: Colors.red),
+  Song(
+      name: "hail to the victor",
+      image: "assets/images/artist1.jpeg",
+      singer: "danito & athina",
+      duration: 532,
+      color: Colors.orange),
+  Song(
+      name: "idk",
+      image: "assets/images/artist3.jpeg",
+      singer: "ley you",
+      duration: 264,
+      color: Colors.blue),
+  Song(
+      name: "like it doesn't hurt",
+      image: "assets/images/artist4.jpeg",
+      singer: "Charle Cardin",
+      duration: 300,
+      color: Colors.grey),
+  Song(
+      name: "love on the brain",
+      image: "assets/images/artist6.jpeg",
+      singer: "Tvorchi",
+      duration: 252,
+      color: Colors.red),
+  Song(
+      name: "hail to the victor",
+      image: "assets/images/artist1.jpeg",
+      singer: "danito & athina",
+      duration: 532,
+      color: Colors.orange),
+  Song(
+      name: "idk",
+      image: "assets/images/artist3.jpeg",
+      singer: "ley you",
+      duration: 264,
+      color: Colors.blue),
+  Song(
+      name: "like it doesn't hurt",
+      image: "assets/images/artist4.jpeg",
+      singer: "Charle Cardin",
+      duration: 300,
+      color: Colors.grey),
+  Song(
+      name: "love on the brain",
+      image: "assets/images/artist6.jpeg",
+      singer: "Tvorchi",
+      duration: 252,
+      color: Colors.red),
+  Song(
+      name: "hail to the victor",
+      image: "assets/images/artist1.jpeg",
+      singer: "danito & athina",
+      duration: 532,
+      color: Colors.orange),
+  Song(
+      name: "idk",
+      image: "assets/images/artist3.jpeg",
+      singer: "ley you",
+      duration: 264,
+      color: Colors.blue),
 ];
